2f0414c96054f21c712b65c092873ea72c8f59a1
boustanihani <boustanihani@users.noreply.github.com>
2015-01-14 16:08:47 +0100

    Update to latest Meteor API
    Update to latest Meteor API

--- meteor-addons.js
+++ meteor-addons.js
@@ -1,13 +1,15 @@
 // Meteor specific addons to the bootbox.js object
 bootbox.templateDialog = function(options) {
-  var div = document.createElement("div");
-  UI.insert(UI.renderWithData(options.template, options.data), div);
-  options.message = div;
+
+  var view = Blaze.renderWithData(options.template, options.data);
+
+  options.message = Blaze.toHTML(view);
 
   var result = bootbox.dialog(options);
-  result.on('done.bootbox.modal', function (e, data) {
+  result.on('done.bootbox.modal', function(e, data) {
     var callback = options.callback;
     if (_.isFunction(callback)) callback(data);
+    Blaze.remove(view);
   });
   return result;
 };
